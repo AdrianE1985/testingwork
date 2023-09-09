0adeac9f9f9638dfb8467faf8f1aec71a2670de0
AdrianE1985 <adrian.eftei@gmail.com>
2023-09-09 19:30:23 +0300

    aaa

--- main_program.py
+++ main_program.py
@@ -10,8 +10,10 @@
 #available_choises=["Abort","build_new_interface","build_new_ospf_neighbor","build_new_bgp_neighbor","update_prefix_list","update_route_map"]
 p = subprocess.Popen(cmd, stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE, shell=True)
+exit_code = p.wait() 
 print("!!!!!!JENKINS WORKS!!!!!", , p.stdout.read().decode())
-exit()
+print("jenkins has errors", p.stderr.read().decode())
+sys.exit(exit_code)
 #router=input()
 #
 #print("Hello user. What will you want to build or edit today ?")
